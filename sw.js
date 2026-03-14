5fc9077169e9e8fb90e3a853ecb4fa1bdd97296b
mirekgorka279-lgtm <mirekgorka279@gmail.com>
2026-03-14 14:38:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('kruszywo-pro').then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
